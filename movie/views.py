07f52662802f0e79501ac53464268d07aad93ac2
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-17 14:41:44 +0530

    Create Signup form
    - Create signup form template
    - Create SignupForm using django UserCreationForm
    - Create views func for signup
    - Add path to views func in movie urls

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from movie.forms import SignupForm
 
 
 def index(request):
     return render(request, "movie/index.html")
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password1 = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password1)
+            if user is not None:
+                login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+                return redirect('index')
+            else:
+                return HttpResponse("Invalid username & password")
+
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
