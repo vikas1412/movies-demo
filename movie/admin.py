9fdec071a72a62e12edec199d199ad762e51156e
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-17 15:48:11 +0530

    Add Genre, Director model & register them

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from movie.models import Director, Genre
+
+
+@admin.register(Genre)
+class DirectorInstanceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp')
+
+
+@admin.register(Director)
+class DirectorInstanceAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'number', 'website')
--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    title = models.CharField(max_length=500, blank=True, null=True)
+    slug = models.SlugField(max_length=100, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Director(models.Model):
+    GENDER = (
+        ('male', 'male'),
+        ('female', 'female'),
+        ('others', 'others'),
+    )
+    first_name = models.CharField(max_length=200, blank=True, null=True)
+    middle_name = models.CharField(max_length=200, blank=True, null=True)
+    last_name = models.CharField(max_length=200, blank=True, null=True)
+    number = models.CharField(max_length=10, blank=True, null=True)
+    dob = models.DateField(max_length=200, blank=True, null=True)
+    website = models.URLField(max_length=230, blank=True, null=True)
+    gender = models.CharField(choices=GENDER, max_length=30, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.number}, {self.website}"
+
