7b7fd128feaded9fdf4ea001ee8690f927d6338a
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-18 23:57:03 +0530

    Change fields, gender choices

--- movie/models.py
+++ movie/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Genre(models.Model):
@@ -12,9 +13,9 @@ class Genre(models.Model):
 
 class Director(models.Model):
     GENDER = (
-        ('male', 'male'),
-        ('female', 'female'),
-        ('others', 'others'),
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('others', 'Others'),
     )
     first_name = models.CharField(max_length=200, blank=True, null=True)
     middle_name = models.CharField(max_length=200, blank=True, null=True)
@@ -50,8 +51,16 @@ class NewMovie(models.Model):
     release_date = models.DateField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     review = models.TextField()
-    genre = models.ManyToManyField('Genre', blank=True)
+    genre = models.ManyToManyField(Genre, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.title} by {self.director.name} released on {self.release_date}"
+        return f"{self.title} by {self.director} released on {self.release_date}"
+
+    # def display_genre(self):
+    #     return ', '.join(genre.title for genre in self.genre.all()[:3])
+    # display_genre.short_description = 'Genre'
+    #
+    # def display_director(self):
+    #     return ', '.join(director.first_name for director in self.director.all()[:3])
+    # display_director.short_description = 'Director'
