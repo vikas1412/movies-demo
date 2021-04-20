634a0fa794dfef8a1d1aa616975b8d8a33422e60
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-20 14:01:20 +0530

    Add view functions, redirect fixes
    - Add new_movie, create_new_director view function.
    - Remove NewMovieCreate, DirectorCreate
    - Render to template

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from movie.forms import SignupForm
+from movie.forms import SignupForm, DirectorForm, MovieForm
 from django.views import generic
 
 from movie.models import Director, Genre, NewMovie
@@ -10,6 +14,57 @@ from movie.models import Director, Genre, NewMovie
 
 def index(request):
     return render(request, "movie/index.html")
+
+
+@transaction.atomic
+@login_required
+def create_new_director(request):
+    form = DirectorForm(request.POST or None)
+
+    if form.is_valid():
+        form_obj = form.save()
+        return redirect('director', form_obj.id)
+
+    params = {
+        'form': form,
+    }
+    return render(request, 'movie/new-director.html', params)
+
+
+class GenreCreate(generic.CreateView):
+    model = Genre
+    template_name = 'movie/new-genre.html'
+    fields = ("title",)
+    success_url = '/'
+
+
+def new_movie(request):
+    form = MovieForm(request.POST, request.FILES or None)
+
+    if form.is_valid():
+        form_obj = form.save(commit=False)
+        form_obj.image = request.FILES['image']
+        form_obj.release_date = datetime.today()
+        form_obj.save()
+        return redirect('movie', form_obj.id)
+
+    params = {
+        'form': form,
+    }
+    return render(request, 'movie/new-movie.html', params)
+
+
+class MoviesList(generic.ListView):
+    model = NewMovie
+    template_name = 'movie/movies.html'
+    context_object_name = "movies"
+    ordering = ['-timestamp']
+
+
+class MovieDetail(generic.DetailView):
+    model = NewMovie
+    template_name = "movie/movie.html"
+    context_object_name = "movie"
 
 
 def signup(request):
@@ -31,34 +86,13 @@ def signup(request):
     return render(request, 'registration/signup.html', {'form': form})
 
 
-class DirectorCreate(generic.CreateView):
+class DirectorList(generic.ListView):
     model = Director
-    template_name = 'movie/new-director.html'
-    fields = "__all__"
-    success_url = "/"
+    template_name = 'movie/directors.html'
+    context_object_name = "directors"
 
 
-class GenreCreate(generic.CreateView):
-    model = Genre
-    template_name = 'movie/new-genre.html'
-    fields = ("title",)
-    success_url = '/'
-
-
-class NewMovieCreate(generic.CreateView):
-    model = NewMovie
-    template_name = 'movie/new-movie.html'
-    fields = "__all__"
-    success_url = "/"
-
-
-class MoviesList(generic.ListView):
-    model = NewMovie
-    template_name = 'movie/movies.html'
-    context_object_name = "movies"
-
-
-class MovieDetail(generic.DetailView):
-    model = NewMovie
-    template_name = "movie/movie.html"
-    context_object_name = "movie"
+class DirectorDetail(generic.DetailView):
+    model = Director
+    template_name = "movie/director.html"
+    context_object_name = "director"
