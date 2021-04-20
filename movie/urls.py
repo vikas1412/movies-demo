6cef18c2689450921c4cbd9638078c611fdf370f
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-20 14:03:24 +0530

    Add new path to view func, new links to template

--- movie/urls.py
+++ movie/urls.py
@@ -7,11 +7,15 @@ urlpatterns = [
 
     path('signup/', views.signup, name="signup"),
 
-    path("new/director/", views.DirectorCreate.as_view(), name="new-director"),
+    path("new/director/", views.create_new_director, name="new-director"),
+
+    path("directors/", views.DirectorList.as_view(), name="directors"),
+
+    path("director/<int:pk>/", views.DirectorDetail.as_view(), name="director"),
 
     path("new/genre/", views.GenreCreate.as_view(), name="new-genre"),
 
-    path("new/movie/", views.NewMovieCreate.as_view(), name="new-movie"),
+    path("new/movie/", views.new_movie, name="new-movie"),
 
     path("all/", views.MoviesList.as_view(), name="movies"),
 
