7ffaaf56eba04a843efb39816ba694e80dab2bcf
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-20 11:20:21 +0530

    Replace document_type to document_root

--- movies/urls.py
+++ movies/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -25,3 +27,6 @@ urlpatterns = [
     path("movies/", include('movie.urls')),
     path("", RedirectView.as_view(url="movies/", permanent=True)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_type=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
